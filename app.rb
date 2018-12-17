969c039f5886ed3de8c8f78f4f77e1323e886d13
Emanuele <emanuelegorga@gmail.com>
2018-12-17 16:31:26 +0000

    Implemented a basic message functionality

--- app.rb
+++ app.rb
@@ -1,15 +1,29 @@
 require 'sinatra/base'
 
 class Messaging < Sinatra::Base
+  enable :sessions
 
   get '/' do
-    # 'Testing Messaging app infrastructure!'
     erb(:index)
   end
 
-  post '/name' do
+  post '/welcome_page' do
     @user_name = params[:user_name]
-    erb(:name)
+    erb(:welcome_page)
+  end
+
+  get '/add_messages' do
+    erb(:add_messages)
+  end
+
+  post '/store_messages' do
+    session[:message] = params[:message]
+    redirect(:show_messages)
+  end
+
+  get '/show_messages' do
+    @show_messages = session[:message]
+    erb(:show_messages)
   end
 
 run! if app_file == $0
